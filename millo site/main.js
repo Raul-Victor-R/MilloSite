eba078b0173baba3f175eb2b164e13c6540388bc
Raul-Victor-R <93218822+Raul-Victor-R@users.noreply.github.com>
2023-03-06 14:06:31 -0300

    Add files via upload

--- millo site/main.js
+++ millo site/main.js
@@ -8,3 +8,18 @@ navLinks.forEach(link => {
     targetSection.scrollIntoView({ behavior: "smooth", block: "start" });
   });
 });
+
+window.onscroll = function() {myFunction()};
+
+var header = document.getElementById("myHeader");
+
+var sticky = header.offsetTop;
+
+// Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+  if (window.pageYOffset > sticky) {
+    header.classList.add("sticky");
+  } else {
+    header.classList.remove("sticky");
+  }
+}
